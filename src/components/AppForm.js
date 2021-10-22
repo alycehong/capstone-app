6daaba71d5a6571e9293723d85d4d1805c8ac436
Alyce <jah6zd@virginia.edu>
2021-10-22 03:16:43 -0400

    removed field type

--- src/components/AppForm.js
+++ src/components/AppForm.js
@@ -1,4 +1,5 @@
 import React from "react";
+// import { useForm } from "react-hook-form";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 
@@ -7,15 +8,15 @@ function AppForm(props) {
         <Form>
           <Form.Group className="mb-3">
             <Form.Label className="text-center">Distance</Form.Label>
-            <Form.Control type="email" placeholder="Enter distance" />
+            <Form.Control placeholder="Enter distance" />
           </Form.Group>
           <Form.Group className="mb-3">
             <Form.Label className="text-center">Direction</Form.Label>
-            <Form.Control type="email" placeholder="Enter direction" />
+            <Form.Control  placeholder="Enter direction" />
           </Form.Group>
           <Form.Group className="mb-3">
             <Form.Label className="text-center">Time</Form.Label>
-            <Form.Control type="password" placeholder="Enter time" />
+            <Form.Control  placeholder="Enter time" />
           </Form.Group>
           <Button variant="primary" type="submit">
             Submit
